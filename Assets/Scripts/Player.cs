fdb07bd257e731a99b8333e769213518113130f9
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-11-30 21:21:30 +1300

    renamed and moved Space Shooter Visual Assets folder
    moved

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,12 +33,10 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject laser = Instantiate
-                (
+            GameObject laser = Instantiate(
                 laserPrefab, 
                 transform.position, 
-                Quaternion.identity
-                ) as GameObject ;
+                Quaternion.identity) as GameObject;
             laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
         }
     }
